fa433460bb6784481ca2cf4d2d2a38f1963593e1
mayavathi <mayachandre27@gmail.com>
2021-07-19 14:52:09 +0530

    find cheapest hotel

--- lib/src/main/java/Hotel/Reservation/System/HotelReservation.java
+++ lib/src/main/java/Hotel/Reservation/System/HotelReservation.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class HotelReservation {
 	private static List<Hotel> hotelList = new ArrayList<>();
 
-    public static boolean addHote(Hotel hotel) {
+    public static boolean addHotel(Hotel hotel) {
         if (hotel == null)
             return false;
         if (hotelList.contains(hotel))
@@ -16,12 +16,27 @@ public class HotelReservation {
         return true;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to hotel reservation program");
+    public static String getCheapestHotel(String... dates) {
+        Hotel cheapestHotel = hotelList.get(0);
+        double cheapestRate = hotelList.get(0).getPrice(dates);
+        for (Hotel hotel : hotelList) {
+            if (hotel.getPrice(dates) < cheapestRate) {
+                cheapestHotel = hotel;
+                cheapestRate = hotel.getPrice(dates);
+            }
+        }
+        return cheapestHotel.getName() + ", Total Rates: $" + Math.round(cheapestRate);
     }
 
-	public static boolean addHotel(Hotel lakewood) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation program");
+        addHotel(new Hotel("Lakewood", 110, 90));
+        addHotel(new Hotel("Bridgewood", 160, 60));
+        addHotel(new Hotel("Ridgewood", 220, 150));
+        Scanner sc = new Scanner(System.in);
+        String[] dates = sc.nextLine().split(",");
+
+        System.out.println(getCheapestHotel(dates));
+
+    }
 }
