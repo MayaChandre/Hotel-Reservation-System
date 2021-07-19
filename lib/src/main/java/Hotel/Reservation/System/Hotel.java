f963c5f8b11260d353893cbf48d646340aa944ec
mayavathi <mayachandre27@gmail.com>
2021-07-19 15:13:25 +0530

    weekend rates

--- lib/src/main/java/Hotel/Reservation/System/Hotel.java
+++ lib/src/main/java/Hotel/Reservation/System/Hotel.java
@@ -2,38 +2,37 @@ package Hotel.Reservation.System;
 
 public class Hotel {
 
-	 private String name;
-	    private double regularWeekdayRates;
-	    private double regularWeekendRates;
+	private String name;
+    private double regularWeekdayRates;
+    private double regularWeekendRates;
 
-	    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates) {
-	        this.name = name;
-	        this.regularWeekdayRates = regularWeekdayRates;
-	        this.regularWeekendRates = regularWeekendRates;
-	    }
+    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates) {
+        this.name = name;
+        this.regularWeekdayRates = regularWeekdayRates;
+        this.regularWeekendRates = regularWeekendRates;
+    }
 
-	    public double getRegularWeekendRates() {
-	        return regularWeekendRates;
-	    }
+    public double getRegularWeekendRates() {
+        return regularWeekendRates;
+    }
 
-	    public void setRegularWeekendRates(double regularWeekendRates) {
-	        this.regularWeekendRates = regularWeekendRates;
-	    }
+    public void setRegularWeekendRates(double regularWeekendRates) {
+        this.regularWeekendRates = regularWeekendRates;
+    }
 
-	    public String getName() {
-	        return name;
-	    }
+    public String getName() {
+        return name;
+    }
 
-	    public double getRegularWeekdayRates() {
-	        return regularWeekdayRates;
-	    }
+    public double getRegularWeekdayRates() {
+        return regularWeekdayRates;
+    }
 
-	    public void setRegularWeekdayRates(double regularWeekdayRates) {
-	        this.regularWeekdayRates = regularWeekdayRates;
-	    }
+    public void setRegularWeekdayRates(double regularWeekdayRates) {
+        this.regularWeekdayRates = regularWeekdayRates;
+    }
 
-	    public double getPrice(String... dates) {
-	        return dates.length * regularWeekdayRates;
-	    }
-	}
-
+    public double getPrice(String... dates) {
+        return dates.length * regularWeekdayRates;
+    }
+}
